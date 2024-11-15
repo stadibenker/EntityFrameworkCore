ebaae8e15223292278b7dbf1973fcfff7558c002
Maxim Bazhenov <stadibenker@gmail.com>
2024-11-15 12:33:19 +0000

    initial data models & db context

---
+++ EntityFrameworkCore.Data/FootballDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EntityFrameworkCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCore.Data
+{
+	public class FootballDbContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=localhost;Database=FootballEFCore;Trusted_Connection=True;TrustServerCertificate=true;MultipleActiveResultSets=true");
+			base.OnConfiguring(optionsBuilder);
+		}
+
+		public DbSet<Team> Teams { get; set; }
+		public DbSet<Coach> Coaches { get; set; }
+	}
+}
---
+++ EntityFrameworkCore.Domain/Coach.cs
@@ -0,0 +1,8 @@
+﻿namespace EntityFrameworkCore.Domain
+{
+	public class Coach : BaseDomainModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
