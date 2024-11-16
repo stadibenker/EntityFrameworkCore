8baaa80b824a7894d77df001169c8df9fada7adf
Maxim Bazhenov <stadibenker@gmail.com>
2024-11-16 13:00:30 +0000

    added default data

--- EntityFrameworkCore.Data/FootballDbContext.cs
+++ EntityFrameworkCore.Data/FootballDbContext.cs
@@ -5,13 +5,20 @@ namespace EntityFrameworkCore.Data
 {
 	public class FootballDbContext : DbContext
 	{
+		public DbSet<Team> Teams { get; set; }
+		public DbSet<Coach> Coaches { get; set; }
+
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseSqlServer("Server=localhost;Database=FootballEFCore;Trusted_Connection=True;TrustServerCertificate=true;MultipleActiveResultSets=true");
 			base.OnConfiguring(optionsBuilder);
 		}
 
-		public DbSet<Team> Teams { get; set; }
-		public DbSet<Coach> Coaches { get; set; }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Team>().HasData(
+				new Team { Id = 1, Name = "Test Team", CreatedDate = new DateTime(2024, 11, 16) }
+			);
+		}
 	}
 }
---
+++ EntityFrameworkCore.Data/Migrations/20241116125955_SeededDefaultTeams.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkCore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededDefaultTeams : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Teams",
+                columns: new[] { "Id", "CreatedDate", "Name" },
+                values: new object[] { 1, new DateTime(2024, 11, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), "Test Team" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Teams",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
